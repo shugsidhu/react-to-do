30572e5431a4fd147d5d35248b9fc06711237b70
Shug Sidhu <Shug@priyas-mbp.attlocal.net>
2018-02-01 20:43:44 -0800

    checkpoint react to do events

--- src/App.js
+++ src/App.js
@@ -3,17 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ],
+       newTodoDescription: ''
+    };
+  }
+
+  handleChange(e) {
+       this.setState({ newTodoDescription: e.target.value })
+     }
+
+  handleSubmit(e) {
+      e.preventDefault();
+      if (!this.state.newTodoDescription) { return }
+      const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+      this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' }); 
+    }
+
+  toggleComplete(index) {
+      const todos = this.state.todos.slice();
+      const todo = todos[index];
+      todo.isCompleted = todo.isCompleted ? false : true;
+      this.setState({ todos: todos });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <ul>
+        { this.state.todos.map( (todo,index) =>
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+        )}
+        </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+            <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+           <input type="submit" />
+         </form>
+        </div>
     );
   }
 }
