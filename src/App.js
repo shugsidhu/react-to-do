9a6c06bd317cc1e5e91b0d7e57278ae1511768c3
Shug Sidhu <Shug@priyas-mbp.attlocal.net>
2018-02-09 21:48:52 -0800

    revision

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
   constructor(props) {
